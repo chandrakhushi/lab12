4c6b5d2149451539bf15adf5c1957b8e743d1e36
Khushi Chandra <khushi.chandra@outlook.com>
2022-11-18 13:09:59 -0800

    second testing of git commands, updated Tree exception, added comment to family tree

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -5,6 +5,7 @@ import java.io.*;
 import javax.swing.*;
 import javax.swing.filechooser.*;
 
+//this is me testing my github stuffff
 
 public class FamilyTree
 {
@@ -33,15 +34,17 @@ public class FamilyTree
         {
             // Add childNode to this node's children list. Also
             // set childNode's parent to this node.
+        	children.add(childNode);
+        	childNode.parent = this;
         }
         
         
         // Searches subtree at this node for a node
         // with the given name. Returns the node, or null if not found.
         TreeNode getNodeWithName(String targetName)
-        {
+        { 
             // Does this node have the target name?
-            if (?????)
+            if (name.equals(targetName))
                 return this;
                     
             // No, recurse. Check all children of this node.
@@ -49,6 +52,9 @@ public class FamilyTree
             {
                 // If child.getNodeWithName(targetName) returns a non-null node,
                 // then that's the node we're looking for. Return it.
+            	if(child.getNodeWithName(targetName) != null) {
+            		return child;
+            	}
             }
             
             // Not found anywhere.
@@ -66,7 +72,14 @@ public class FamilyTree
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
-
+            TreeNode curr = this.parent;
+            
+            while(curr != null) {
+            	
+            	ancestors.add(curr);
+            	curr = curr.parent;
+            }
+            
             return ancestors;
         }
         
